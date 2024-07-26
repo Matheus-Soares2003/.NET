b04ad7897c3922b96b1749184f9462f5649fe44c
Matheus Cabral <matheusstcabral@gmail.com>
2024-07-26 11:42:03 -0300

    Novo arquivo - Estudando as condicionais em C#

---
+++ Condicoes/Condicoes/Program.cs
@@ -0,0 +1,60 @@
+﻿//CONDICIONAIS SIMPLES
+
+Console.Write("Digite um número: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 10)
+{
+    Console.WriteLine($"{num} é maior que 10");
+} 
+else if (num < 10)
+{
+    Console.WriteLine($"{num} é menor que 10");
+}
+else
+{
+    Console.WriteLine($"{num} é igual a 10");
+}
+Console.WriteLine("\n\n");
+
+
+//SWITCH CASE
+
+Console.WriteLine("1 - Somar 5\n" +
+    "2 - Subtrair 5\n" +
+    "3 - Multiplicar por 5\n" +
+    "4 - Dividir por 5");
+
+Console.Write("Opção: ");
+int opcao = Convert.ToInt32(Console.ReadLine());
+
+switch (opcao)
+{
+    case 1:
+        num += 5;
+        break;
+
+    case 2:
+        num -= 5;
+        break;
+
+    case 3:
+        num *= 5;
+        break;
+
+    case 4:
+        num /= 5;
+        break;
+
+    default:
+        Console.WriteLine("Opção Inválida!");
+        break;
+}
+Console.WriteLine($"\nNum = {num}");
+
+//OPERADORES TERNÁRIOS
+
+Console.Write("\nIdade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(idade >= 18 ? "Maior de Idade" : "Menor de Idade"); //Nesse caso a primeira string será escrita caso idade seja maior ou igual a 18, senão a segunda string será escrita
